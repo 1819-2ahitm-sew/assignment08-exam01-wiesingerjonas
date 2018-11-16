c215fddb08d10189dd1da351c59bdd3f11c72ad1
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2018-11-16 10:38:07 +0100

    Zweites Zwischenergebnis

--- src/main/java/at/htl/exam01/document/Main.java
+++ src/main/java/at/htl/exam01/document/Main.java
@@ -2,25 +2,6 @@ package at.htl.exam01.document;
 
 public class Main {
 
-    /**
-     *
-     * Führen Sie hier folgendes durch:
-     *
-     * 1. Erstellen Sie 2 Bücher und ein email
-     *    Buch: "Rowlings", "Harry Potter und der Stein der Weisen"
-     *    Email: "Susi", "Bewerbung", "CoolCompany"
-     *    Buch: "Tolkien", "lordOfTheRings"
-     *
-     * 2. Erstellen Sie ein Document-Array und speichern Sie obige Dokumente in diesem Array.
-     *
-     * 3. Geben Sie die Inhalte dieser Documente wie folgt aus.
-     *    Nutzen Sie dabei die Mechanismen der Vererbung.
-     *    Erstellen Sie außerdem geeignete toString-Methoden
-     *
-     * 4. Zählen Sie die Bücher und emails im Array und geben Sie die jeweilige Anzahl aus.
-     *
-     * @param args
-     */
     public static void main(String[] args) {
 
         Book book1 = new Book("Rowling", "Harry Potter und der Stein der Weisen");
@@ -32,9 +13,19 @@ public class Main {
         objects[1] = email1;
         objects[2] = book2;
 
+        int anzahlBook = 0;
+        int anzahlEmail = 0;
+
         for (int i = 0; i < objects.length; i++) {
             if(objects[i].getClass().toString().equals("class at.htl.exam01.document.Book")){
-                System.out.println("Book: '" + objects[i].);
+
+                if(anzahlBook == 1){
+                    System.out.println("Book:  '" + book1.title + "' von '" + book2.author + "'");
+                }
+                anzahlBook++;
+            }else{
+                System.out.println("Email: '");
+                anzahlEmail++;
             }
         }
 
